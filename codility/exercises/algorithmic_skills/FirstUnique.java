374b8dd35eb8f4ceb30435d34bf970ad4e14a473
aleandraca <m.alejandro.andraca@gmail.com>
2024-06-24 18:25:23 -0600

    Aadd new solution for codility First Unique excersice.

--- codility/exercises/algorithmic_skills/FirstUnique.java
+++ codility/exercises/algorithmic_skills/FirstUnique.java
@@ -3,18 +3,31 @@ import java.util.*;
 /**
  * Codility's First Unique excersice. 100% java 11
  * https://app.codility.com/programmers/trainings/4/first_unique/
+ * In this exercise the key concept is using a **LinkedHashMap**
+ * which ensures insertion order. I included 2 versions of the solution.
  */
 public class FirstUnique {
   public static void main(String[] args) {
     var a = new int[] { 4, 10, 5, 4, 2, 10 };
-    System.out.println(Arrays.toString(a) + " -> " + solution(a));
+    System.out.println(Arrays.toString(a) + " -> " + solution1(a));
     var b = new int[] { 1, 4, 3, 3, 1, 2 };
-    System.out.println(Arrays.toString(b) + " -> " + solution(b));
+    System.out.println(Arrays.toString(b) + " -> " + solution1(b));
     var c = new int[] { 6, 4, 4, 6 };
-    System.out.println(Arrays.toString(c) + " -> " + solution(c));
+    System.out.println(Arrays.toString(c) + " -> " + solution1(c));
   }
 
-  public static int solution(int[] a) {
+  // Functional approach with streams
+  public static int solution1(int[] a) {
+    var map = new LinkedHashMap<Integer, Integer>();
+    Arrays.stream(a).forEach(n -> map.merge(n, 1, Integer::sum));
+    return map.entrySet().stream()
+        .filter(e -> e.getValue() == 1)
+        .map(e -> e.getKey())
+        .findFirst()
+        .orElse(-1);
+  }
+
+  public static int solution2(int[] a) {
     var map = new LinkedHashMap<Integer, Integer>();
     for (int n : a) {
       map.merge(n, 1, Integer::sum);
