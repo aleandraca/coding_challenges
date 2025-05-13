751e8a188198ac72e99c8ff4071ad2dc1a457825
Ale Andraca <aleandraca@outlook.com>
2025-05-13 10:48:53 -0600

    Add another solution to FrogJmp.

--- codility/lessons/03_time_complexity/FrogJump.java
+++ codility/lessons/03_time_complexity/FrogJump.java
@@ -1,12 +1,17 @@
 public class FrogJump {
   public static void main(String[] args) {
-    var ac = solution(10, 85, 30);
+    var ac = solution2(10, 85, 30);
     var ex = 3;
     System.out.println((ex == ac ? "🟢" : "🔴") + " Result -> " + ac);
   }
 
   // 100% O(1)
   public static int solution(int x, int y, int d) {
-    return (int)Math.ceil((y - x) / (double)d);
+    return (int) Math.ceil((y - x) / (double) d);
+  }
+
+  // 100% pure integer division
+  public static int solution2(int x, int y, int d) {
+    return (y - x + d - 1) / d;
   }
 }
