65cbdfa14938a489c9a10acadef17f5d70bcf63e
Ale Andraca <aleandraca@outlook.com>
2025-06-01 18:25:54 -0600

    Update codility's CyclicRotation.

--- codility/lessons/02_arrays/CyclicRotation.java
+++ codility/lessons/02_arrays/CyclicRotation.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.function.*;
 
 /**
  * Codility Lesson 2 / Arrays / CyclicRotation
@@ -8,16 +9,33 @@ import java.util.Arrays;
  * I also included another less performant solution O(n*k).
  */
 public class CyclicRotation {
-  public static void main(String[] args) {
-    // A tricky scenario when k > length of the array.
-    var a = new int[] { 8, 5, 2 };
-    int k = 4;
-    System.out.println(Arrays.toString(a) + " -> "
-        + Arrays.toString(solution1(a, k)));
+
+  // 100% O(n) time complexity O(1) space complexity
+  // Using the reversal algorithm to achieve constant space complexity
+  public int[] solution(int[] nums, int k) {
+    int n = nums.length;
+    if (n == 0 || k % n == 0) {
+      return nums;
+    }
+    k %= n;
+    BiConsumer<Integer, Integer> reverseSubArray = (start, end) -> {
+      while (start < end) {
+        int lastElem = nums[end];
+        nums[end] = nums[start];
+        nums[start] = lastElem;
+        start++;
+        end--;
+      }
+    };
+    reverseSubArray.accept(n - k, n - 1);
+    reverseSubArray.accept(0, n - 1 - k);
+    reverseSubArray.accept(0, n - 1);
+    return nums;
   }
 
   // 100% O(n) Making use of modular arithmetic
-  public static int[] solution1(int[] a, int k) {
+  // O(n) space complexity
+  public int[] solution2(int[] a, int k) {
     int n = a.length;
     if (n < 2 || k % n == 0) {
       return a;
@@ -30,7 +48,7 @@ public class CyclicRotation {
   }
 
   // 100% O(k*n) we rotate the array step by step.
-  public static int[] solution2(int[] a, int k) {
+  public int[] solution3(int[] a, int k) {
     int n = a.length;
     if (n == 0 || k % n == 0) {
       return a;
@@ -46,5 +64,25 @@ public class CyclicRotation {
       }
     }
     return b;
+  }
+
+  public static void main(String[] args) {
+    test(new TestCase(new int[] { 8, 5, 2 }, 4, new int[] { 2, 8, 5 }));
+    test(new TestCase(new int[] { 3, 8, 9, 7, 6 }, 3, new int[] { 9, 7, 6, 3, 8 }));
+    test(new TestCase(new int[] {}, 1, new int[] {}));
+    test(new TestCase(new int[] { 1 }, 0, new int[] { 1 }));
+    test(new TestCase(new int[] { 1, 2, 3, 4 }, 8, new int[] { 1, 2, 3, 4 }));
+  }
+
+  private record TestCase(int[] nums, int k, int[] expected) {
+  }
+
+  private static void test(TestCase testCase) {
+    var app = new CyclicRotation();
+    var actual = app.solution(testCase.nums, testCase.k);
+    System.out.println((Arrays.equals(actual, testCase.expected) ? "🟢 " : "🔴 ")
+        + Arrays.toString(testCase.nums) + " k: " + testCase.k + "\n  ↪️"
+        + " Out " + Arrays.toString(actual)
+        + " | Expected " + Arrays.toString(testCase.expected));
   }
 }
