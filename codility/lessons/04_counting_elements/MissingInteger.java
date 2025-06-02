6e562cdd59c1f323ec3589c89010d6c489a487e8
Ale Andraca <aleandraca@outlook.com>
2025-06-02 00:18:10 -0600

    Update codility's MissingInteger.

--- codility/lessons/04_counting_elements/MissingInteger.java
+++ codility/lessons/04_counting_elements/MissingInteger.java
@@ -7,7 +7,7 @@ import java.util.HashSet;
 public class MissingInteger {
   public static void main(String[] args) {
     var a = new int[] { 1, 3, 6, 4, 1, 2 };
-    int actual = solution(a);
+    int actual = solution2(a);
     int expected = 5;
     System.out.println((actual == expected ? "🟢" : "🔴")
         + " Result " + actual
@@ -15,19 +15,32 @@ public class MissingInteger {
   }
 
   // 100%
-  public static int solution(int[] a) {
+  public static int solution(int[] nums) {
     var set = new HashSet<Integer>();
-    int max = Integer.MIN_VALUE;
-    for (int num : a) {
+    for (int num : nums) {
       set.add(num);
-      max = Math.max(num, max);
     }
-    int i = 1;
-    for (; i <= max; i++) {
+    for (int i = 1; i <= nums.length; i++) {
       if (!set.contains(i)) {
         return i;
       }
     }
-    return max <= 0 ? 1 : i;
+    return nums.length + 1;
+  }
+
+  // Array solution 100%
+  public static int solution2(int[] nums) {
+    boolean[] seen = new boolean[nums.length + 1];
+    for (int num : nums) {
+      if (num > 0 && num <= nums.length) {
+        seen[num] = true;
+      }
+    }
+    for (int i = 1; i <= nums.length; i++) {
+      if (!seen[i]) {
+        return i;
+      }
+    }
+    return nums.length + 1;
   }
 }
